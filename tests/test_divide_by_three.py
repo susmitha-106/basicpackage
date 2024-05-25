61d9b094604490817c7d5c875a4fcdb78af64aef
susmitha <pillisusmitha097@gmail.com>
2024-05-25 15:02:17 +0530

    created basicpackage

---
+++ tests/test_divide_by_three.py
@@ -0,0 +1,7 @@
+import unittest
+from divide.by_three import divide_by_three
+
+class TestDivideByThree(unittest.TestCase):
+    def test_divide_by_three(self):
+        self.assertEqual(divide_by_three(12), 4)
+unittest.main()
